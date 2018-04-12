f93734f9cd1838066da491a187141086471f48bc
Pete Burns <peterburnshicks@gmail.com>
2018-04-11 22:00:55 -0400

    Building for production

--- client/src/pages/StartGame.js
+++ client/src/pages/StartGame.js
@@ -7,6 +7,9 @@ import Toggle from 'material-ui/Toggle';
 import ChessBoard from "chessboardjs";
 import '../chessboard-0.3.0.css';
 import $ from 'jquery';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import JoinButton from '../components/JoinButton';
+import { ListItem } from 'material-ui/List';
 
 window.$ = $;
 window.jQuery = $;
@@ -14,13 +17,17 @@ window.jQuery = $;
 class StartGame extends Component {
     state = {
         white: true,
-        board: null
+        board: null,
+        games: []
     }
 
     componentDidMount() {
         const board = ChessBoard('board', 'start');
         $(window).resize(board.resize);
         this.setState({ board: board });
+        axios.get('/api/games/join')
+            .then(res => this.setState({ games: res.data }))
+            .catch(err => console.log(err));
     }
     changeColor(event, isChecked) {
         this.setState({ white: isChecked });
@@ -34,30 +41,65 @@ class StartGame extends Component {
             .then(history.push('/mygames'))
             .catch(err => console.log(err));
     }
+    handleChange = (value) => {
+        this.setState({
+            value: value,
+        });
+    }
+    joinGame(gameId, color) {
+        axios.put('api/games/join/' + gameId, { color: color })
+            .then(() => this.props.history.push('/mygames'))
+            .catch(err => console.log(err));
+    }
     render() {
         return (
-            <Grid fluid>
-                <Row center="md">
-                    <Col lg={4} md={6} sm={9} xs={12} >
-                        <form onSubmit={(event) => this.createGame(event)}>
-                            <div id="board" style={{ width: '100%', maxWidth: '600px' }} />
-                            <div>
-                                <Toggle
-                                    style={{ maxWidth: 250 }}
-                                    // label={this.state.white ? 'White' : 'Black'}
-                                    toggled={this.state.white}
-                                    thumbStyle={{ backgroundColor: 'black' }}
-                                    trackStyle={{ backgroundColor: 'gery' }}
-                                    thumbSwitchedStyle={{ backgroundColor: 'white' }}
-                                    trackSwitchedStyle={{ backgroundColor: 'grey' }}
-                                    onToggle={(event, isChecked) => this.changeColor(event, isChecked)}
-                                />
-                            </div>
-                            <div>
-                                <FlatButton hoverColor="#994d00" backgroundColor='#663300' style={{ color: '#fff3e6', fontFamily: "'Montserrat', sans-serif" }} label='Create Game' type="submit">
-                                </FlatButton>
-                            </div>
-                        </form>
+            <Grid >
+                <Row center="xs">
+                    <Col xs={12}>
+                        <Tabs
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            tabItemContainerStyle={{ background: "#663300" }}
+                            inkBarStyle={{ background: "#ffb366" }}>
+                            <Tab value="start" label="Start Game">
+                                <Row center="xs">
+                                    <Col center="xs" xs={12} >
+                                        <form onSubmit={(event) => this.createGame(event)}>
+                                            <div id="board" style={{ width: '100%', maxWidth: '600px', margin: '0 auto' }} />
+                                            <span>
+                                                <Toggle
+                                                    style={{ maxWidth: 50, margin: '0 auto' }}
+                                                    toggled={this.state.white}
+                                                    thumbStyle={{ backgroundColor: 'black' }}
+                                                    trackStyle={{ backgroundColor: 'grey' }}
+                                                    thumbSwitchedStyle={{ backgroundColor: 'white' }}
+                                                    trackSwitchedStyle={{ backgroundColor: 'grey' }}
+                                                    onToggle={(event, isChecked) => this.changeColor(event, isChecked)}
+                                                />
+                                            </span>
+                                            <div>
+                                                <FlatButton hoverColor="#994d00" backgroundColor='#663300' style={{ color: '#fff3e6', fontFamily: "'Montserrat', sans-serif" }} label='Create Game' type="submit">
+                                                </FlatButton>
+                                            </div>
+                                        </form>
+                                    </Col>
+                                </Row>
+                            </Tab>
+                            <Tab value="join" label="Join Game">
+                                <Row center="xs">
+                                    {!this.state.games[0] ?
+                                        <p style={{ color: "#663300", fontFamily: "'Montserrat', sans-serif" }}> No joinable games, would you like to <FlatButton label="start one?" onClick={()=>this.setState({value:'start'})}hoverColor="#994d00" backgroundColor='#663300' style={{ color: '#fff3e6', fontFamily: "'Montserrat', sans-serif" }} /></p>
+                                        : this.state.games.map(game => (
+                                            <Col key={game._id} md={9} xs={12}>
+                                                <ListItem>
+                                                    <span>White: {game.whitePlayer ? game.whitePlayer.username : <JoinButton onClick={() => this.joinGame(game._id, 'whitePlayer')} />} </span>
+                                                    <span>Black: {game.blackPlayer ? game.blackPlayer.username : <JoinButton onClick={() => this.joinGame(game._id, 'blackPlayer')} />} </span>
+                                                </ListItem>
+                                            </Col>
+                                        ))}
+                                </Row>
+                            </Tab>
+                        </Tabs>
                     </Col>
                 </Row>
             </Grid>
